f8dfb1868651be081f7bfe520cbfaa6ab8d53aec
frequencycs <58235292+fpsheaven@users.noreply.github.com>
2022-11-17 11:26:12 +0200

    Python Source code

---
+++ pubg_movie_remover.py
@@ -0,0 +1,33 @@
+import os
+import time
+import webbrowser
+print ("Welcome to PUBG MOVIE remover. Follow the steps below.")
+if os.path.exists("C:\movie_remover") == 0:
+    os.mkdir("C:\movie_remover")
+else:
+    pass
+os.chdir("C:\movie_remover")
+txtpath="C:\movie_remover\path.txt"
+
+if os.path.exists("C:\Program Files (x86)\Steam\steamapps\common\PUBG\TslGame\Movies"):
+    print ("Removing movie files from PUBG folder.")
+    os.rmdir("C:\Program Files (x86)\Steam\steamapps\common\PUBG\TslGame\Movies")
+    time.sleep(2) # Sleep for 3 seconds
+    print ("MOVIES REMOVED.Follow @FREQUENCYCS on twitter for cool WINDOWS FPS optimizations :) ")
+    webbrowser.open_new_tab("https://twitter.com/fREQUENCYCS")
+
+elif os.path.exists(txtpath):
+    f = open("path.txt", "r")
+    path2=f.readline()
+    f.close()
+    os.rmdir(path2)
+    
+else:
+    path=input("Cant find PUBG.If have pubg installed on an other drive,paste the movie dir here ")
+    f = open("path.txt", "w")
+    f.write(path)
+    f.close()
+    os.rmdir(path)
+    
+    
+
